2614b6a440124f3ecaf2928b715e76ca88cadcd1
Nordy Vlasman <nordy@snakeware.nl>
2018-09-18 08:36:43 +0200

    Geen idee.

--- dev_bord/src/app.js
+++ dev_bord/src/app.js
@@ -1,43 +1,19 @@
-/**
- * Copyright 2016 Google Inc. All Rights Reserved.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/license-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the license.
- */
-
-
 const anypixel = require('anypixel');
 const ctx = anypixel.canvas.getContext2D();
 
 const ws = new WebSocket("ws://localhost:8080", "protocolOne");
-// const Pong = require('./Pong.js');
 
-let colors = ['#F00', '#0F0', '#00F'];
+let colors = ['#247BA0', '#FF1654'];
 let counter = 0;
 let letters = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ").split("");
 
 drawLetter(letters[counter]);
-
-// ctx.clearRect(0, 0, anypixel.config.width,  anypixel.config.height);
-/**
- * Listen for onButtonDown events and draw a 2x2 rectangle at the event site
- */
 
 ws.onopen = (event) => {
     console.log("Ready");
 };
 
 ws.onmessage = (event) => {
-    console.log(event.data);
     if(event.data === "click") drawLetter(letters[++counter]);
 };
 
@@ -46,16 +22,15 @@ document.addEventListener('onButtonDown', function(event) {
 });
 
 function drawLetter(letter) {
-
     ctx.clearRect(0, 0, anypixel.config.width, anypixel.config.height);
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, anypixel.config.width, anypixel.config.height);
 
     ctx.beginPath();
-    ctx.fillStyle = colors[Math.floor(Math.random() * 3)];
-    ctx.font = "15px 'anypixelfont'";
+    ctx.fillStyle = colors[Math.floor(Math.random() * 2)];
+    ctx.font = "20px 'anypixelfont'";
 
-    ctx.fillText(letter, 0, anypixel.config.height - 10);
+    ctx.fillText(letter, anypixel.config.width / 2 -5, anypixel.config.height - 15);
     ctx.closePath();
 
     if(counter === letters.length - 1) counter = -1;
