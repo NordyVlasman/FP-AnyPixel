ef64c666234ea654cd58d8972830213d3fa89d68
Nordy Vlasman <nordy@snakeware.nl>
2018-09-24 17:32:15 +0200

    Refactor 2.0

--- pong/src/app.js
+++ pong/src/app.js
@@ -8,20 +8,23 @@ const ws = new WebSocket("ws://localhost:8080", "protocolOne");
 
 ws.onopen = (event) => {
     ws.send()
-    console.log("Ready");
 };
 
 ws.onmessage = (event) => {
-    const btnEvent = new Event("onButtonDown");
-    var test = JSON.parse(event.data);
-    console.log(test.msg);
-    btnEvent.detail = {x: 0, y: ((test.msg === "buttonUp" ? 0 : 100000000000))};
-    document.dispatchEvent(btnEvent);
+    const btnDownEvent = new Event("onButtonDown");
+    var clientMessage = JSON.parse(event.data);
+    btnDownEvent.detail = {
+        x: 0,
+        y: ((clientMessage.msg === "buttonUp" ? 0 : 100000000000))
+    };
+
+    document.dispatchEvent(btnDownEvent);
+
     setTimeout(
         () => {
-            const event2 = new Event("onButtonUp");
-            event2.detail = {x: 0};
-            document.dispatchEvent(event2);
+            const buttonUpEvent = new Event("onButtonUp");
+            buttonUpEvent.detail = {x: 0};
+            document.dispatchEvent(buttonUpEvent);
         }, 50
     )
 };
@@ -30,7 +33,6 @@ ws.onmessage = (event) => {
  * Listen for onButtonDown events and draw a 2x2 rectangle at the event site
  */
 document.addEventListener('onButtonDown', function(event) {
-
     if(event.detail.x < anypixel.config.width * playerBoardWidth) {
         game.players[0].vel.y = (event.detail.y > anypixel.config.height / 2 ? -game.players[0].velSpeed : game.players[0].velSpeed);
     } else if(event.detail.x > anypixel.config.width * (1-playerBoardWidth)) {
